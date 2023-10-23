b897f746998d5e713d0291f84defa42c5d883c3c
alfettox <dott.defranceschi@gmail.com>
2023-10-22 20:30:19 -0400

    Add simple docker practice

--- Python/pythonTest/main.py
+++ Python/pythonTest/main.py
@@ -2,42 +2,40 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-valid_username = os.getenv("VALID_USERNAME")
-valid_password = os.getenv("VALID_PASSWORD")
-invalid_username = os.getenv("INVALID_USERNAME")
-invalid_password = os.getenv("INVALID_PASSWORD")
+valid_username = "tomsmith"
+valid_password = "SuperSecretPassword!"
+invalid_username = "USERNAME"
+invalid_password = "PASSWORD"
 
 driver = webdriver.Chrome()
 
-login_url = "https://www.saucedemo.com" 
+login_url = "https://opensource-demo.orangehrm.com/"
+
 
 def perform_login(username, password):
     driver.get(login_url)
-
-    username_field = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, "user[email]"))
-    )
-
-    password_field = driver.find_element(By.NAME, "user[password]")
-
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'user-name')))
+    username_field = driver.find_element(By.ID, "user-name")
+    password_field = driver.find_element(By.ID, "password")
     username_field.send_keys(username)
     password_field.send_keys(password)
+    password_field.submit()
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'inventory_container')))
+    result = driver.page_source
+    print(result)
+    print("-----------------------")
 
-    login_button = driver.find_element(By.NAME, "commit")
-    login_button.click()
 
-    if "dashboard" in driver.current_url:
-        print(f"Success: Logged in as {username}")
-    else:
-        print(f"Failed: Unable to log in as {username}")
+
 
 print("Testing Login Attempts:")
+print("Valid Username:", valid_username)
+print("Valid Password:", valid_password)
 print("-----------------------")
 perform_login(valid_username, valid_password)
 perform_login(invalid_username, valid_password)
